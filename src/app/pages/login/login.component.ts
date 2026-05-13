e94c14a1678a2e2c9a3f5923927f44ad0fa37ae4
fpolania <lassofabian36@gmail.com>
2026-05-13 18:04:32 -0500

    ajustes boton login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,24 +1,40 @@
 import { Component, inject } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
-import { AuthService } from '../../core/services/auth.service';
-import Swal from 'sweetalert2';
+
 import { RouterLink } from '@angular/router';
+
+import Swal from 'sweetalert2';
+
+import { AuthService } from '../../core/services/auth.service';
+
 import { TeamPlayersService } from '../../core/services/team-players.service';
+
 declare const bootstrap: any;
 
 @Component({
   selector: 'app-login',
+
   standalone: true,
+
   imports: [CommonModule, RouterLink],
+
   templateUrl: './login.component.html',
+
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  /* INJECTS */
+
   authService = inject(AuthService);
+
   private teamPlayersService = inject(TeamPlayersService);
+  validatedPlayer = false;
+
   async login() {
     await this.authService.loginWithGoogle();
   }
+
   async showDocumentPrompt() {
     const opened = document.querySelector(
       '.offcanvas.show',
@@ -34,22 +50,35 @@ export class LoginComponent {
       await new Promise((r) => setTimeout(r, 250));
     }
 
-    const { value: userDocument, isDismissed } = await Swal.fire({
+    const {
+      value: userDocument,
+
+      isDismissed,
+    } = await Swal.fire({
       title: 'Acceso al equipo ⚽',
+
       input: 'text',
+
       inputLabel: 'Ingresa tu Llave',
+
       inputPlaceholder: 'Número de llave',
+
       inputAttributes: {
         maxlength: '20',
+
         autocapitalize: 'off',
+
         autocorrect: 'off',
       },
-      confirmButtonText: 'Continuar',
+
+      confirmButtonText: 'Validar acceso',
+
       cancelButtonText: 'Cancelar',
+
       showCancelButton: true,
+
       returnFocus: false,
     });
-
     if (isDismissed || !userDocument) return;
     const normalizedDocument = userDocument.trim();
     try {
@@ -63,13 +92,22 @@ export class LoginComponent {
         });
         return;
       }
+
       sessionStorage.setItem('teamPlayer', JSON.stringify(player));
-      this.login();
+      this.validatedPlayer = true;
+      await Swal.fire({
+        icon: 'success',
+        title: 'Acceso aprobado',
+        text: 'Ahora continúa con Google 🚀',
+      });
     } catch (error) {
       console.error(error);
+
       await Swal.fire({
         icon: 'error',
+
         title: 'Oops...',
+
         text: 'Error validando acceso.',
       });
     }
